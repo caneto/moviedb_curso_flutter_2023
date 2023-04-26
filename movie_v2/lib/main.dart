977125b5005f393a4c355073df91f592f23bfddf
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-04-26 15:06:52 -0300

    Atualização

--- movie_v2/lib/main.dart
+++ movie_v2/lib/main.dart
@@ -1,18 +1,63 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:movie_v2/app/core/model/movie_list.dart';
+import 'package:movie_v2/app/core/model/movie_list_error.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  MoviesList? movies;
+  MoviesListError? moviesListError;
+
+  Future<void> _getListagemAPI() async {
+    http.get(
+      Uri.https('api.themoviedb.org', '/4/list/1'),
+      headers: {
+        'authorization': 'Bearer <<token_api>>',
+        'content-type': 'application/json;charset=utf-8'
+      },
+    ).then(
+      (Response value) {
+        if (value.statusCode == 200) {
+          movies = MoviesList.fromJson(
+            jsonDecode(value.body),
+          );
+        } else if ([401, 404, 500].contains(value.statusCode)) {
+          moviesListError = MoviesListError.fromJson(
+            jsonDecode(value.body),
+          );
+        }
+      },
+    ).whenComplete(() => setState(() {}));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getListagemAPI();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         body: Center(
-          child: Text('Hello World!'),
+          child: moviesListError != null
+              ? Text(moviesListError.toString())
+              : Text('Movie: ${movies?.name ?? "Erro"}'),
         ),
       ),
     );
